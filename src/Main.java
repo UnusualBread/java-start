9418141acd60f341354e86e82ae337e6e805ddb1
Tyomik <tyomik@MacBook-Pro-admin.local>
2025-06-23 08:17:50 +0300

    Save before rebase

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int numApples = 1000000;
+        int numPackedApples = 1_000_000;
+        char c = '\0';
+        String s = "edd";
+        System.out.println("Hello, world!");
+        System.out.println(numApples + numPackedApples);
+        System.out.println(c);
+        System.out.println(s);
     }
 }
