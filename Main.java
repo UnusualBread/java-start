ea6b666388f806d165ec244b3fb2250d6c11cd81
Tyomik <tyomik@MacBook-Pro-admin.local>
2025-06-23 07:44:01 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int numApples = 1000000;
+        int numPackedApples = 1_000_000;
+        char c = '\0';
+        String s = "edd";
+        System.out.println("Hello, world!");
+        System.out.println(numApples + numPackedApples);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
