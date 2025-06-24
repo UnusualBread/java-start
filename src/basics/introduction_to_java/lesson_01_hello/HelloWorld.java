b7e63c3b447c779c8b6e8acdc254506e20cb2975
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-24 13:12:44 +0300

    Remove Main.java, add README and initial folder structure

---
+++ src/basics/introduction_to_java/lesson_01_hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package basics.introduction_to_java.lesson_01_hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ src/basics/introduction_to_java/lesson_02_basic_literals/BasicLiterals.java
@@ -0,0 +1,5 @@
+package basics.introduction_to_java.lesson_02_basic_literals;
+
+public class BasicLiterals {
+
+}
