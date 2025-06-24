6f309b3c43bb8a201e6a97c0e62c7c83f89e6b82
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-24 17:12:29 +0300

    Add .gitignore and remove extra basics folder

--- basics/introduction_to_java/lesson_01_hello/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package basics.introduction_to_java.lesson_01_hello;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
