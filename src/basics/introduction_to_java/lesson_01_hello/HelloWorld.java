142df3daa81c2e5cd12f9b3fd7dc33a500d6d07a
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-24 17:01:58 +0300

    Restore extra src folder

--- src/basics/introduction_to_java/lesson_01_hello/HelloWorld.java
+++ src/basics/introduction_to_java/lesson_01_hello/HelloWorld.java
@@ -2,6 +2,6 @@ package basics.introduction_to_java.lesson_01_hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, World!");
     }
 }
