62efe5a94ce09d195c5f1e08e2a2812c45a7b131
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-25 23:11:45 +0300

    Add T03_AssifnValues02

--- src/C01_basics/M02_types_and_variables/L01_assigning_values/T03_AssignValues02.java
+++ src/C01_basics/M02_types_and_variables/L01_assigning_values/T03_AssignValues02.java
@@ -1,4 +1,15 @@
 package C01_basics.M02_types_and_variables.L01_assigning_values;
-
+/*
+Change the code sample to make the output look the following way:
+3 5 4
+To do that, assign the correct values to the corresponding variables!
+*/
 public class T03_AssignValues02 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 4;
+
+        System.out.println(a + " " + b + " " + c);
+    }
 }
