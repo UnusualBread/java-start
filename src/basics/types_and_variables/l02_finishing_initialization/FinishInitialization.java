0978e18d47c4aed48367eaa98cada1e18a4bbab6
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-25 13:15:53 +0300

    Add lesson 2 FinishInitialization.java

---
+++ src/basics/types_and_variables/l02_finishing_initialization/FinishInitialization.java
@@ -0,0 +1,12 @@
+package basics.types_and_variables.l02_finishing_initialization;
+/*
+Given a variable number, add the int type and assign the value 123456 to it.
+Do not forget ; at the end of the statement.
+ */
+public class FinishInitialization {
+    public static void main(String[] args) {
+        int number = 123456;
+
+        System.out.println(number);
+    }
+}
