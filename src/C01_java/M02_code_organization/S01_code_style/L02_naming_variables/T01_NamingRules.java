bd8f5d0b8eb7e24138c8a4a608d4866d8c9ea855
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-26 02:39:14 +0300

    Add file with rules of naming variables

---
+++ src/C01_java/M02_code_organization/S01_code_style/L02_naming_variables/T01_NamingRules.java
@@ -0,0 +1,14 @@
+package C01_java.M02_code_organization.S01_code_style.L02_naming_variables;
+
+public class T01_NamingRules {
+    public static void main(String[] args) {
+        /*
+        1. names are case-sensitive;
+        2. a name can include Unicode letters, digits,
+        and two special characters ($, _);
+        3. a name cannot start with a digit;
+        4. a name must not be a keyword (class, static,
+        int, etc. are illegal names).
+        */
+    }
+}
