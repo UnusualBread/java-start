59973ffed4b3ca363ecd8aab94745b59814817c4
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-26 02:17:50 +0300

    Add task on summing two integers from input

--- src/C01_java/M01_basics/S03_simple_programs/L01_reading_via_scanner/T01_ReadInput.java
+++ src/C01_java/M01_basics/S03_simple_programs/L01_reading_via_scanner/T01_ReadInput.java
@@ -2,6 +2,12 @@ package C01_java.M01_basics.S03_simple_programs.L01_reading_via_scanner;
 
 import java.util.Scanner;
 
+/*Create a program that accepts a user's name and age as input and prints out
+a welcome message to the user, referencing their name and age. If the user's
+name is 'John' and their age is '22', the program should print
+'Hello, I am John! I am 22 years old.'
+ */
+
 public class T01_ReadInput {
     public static void main(String[] args) {
         // Initialize the Scanner object to receive user input
