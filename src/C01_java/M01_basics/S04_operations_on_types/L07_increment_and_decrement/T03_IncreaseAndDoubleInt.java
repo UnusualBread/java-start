455f9fa6bccc5419b92927789b05fd152e127599
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-26 20:02:33 +0300

    Add file for increasing and doubling digit

---
+++ src/C01_java/M01_basics/S04_operations_on_types/L07_increment_and_decrement/T03_IncreaseAndDoubleInt.java
@@ -0,0 +1,26 @@
+package C01_java.M01_basics.S04_operations_on_types.L07_increment_and_decrement;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Given an integer n, print the value of n after applying the following operations:
+increment n by 1, then decrement n by 2, then double the resulting value of n.
+*/
+
+public class T03_IncreaseAndDoubleInt {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // Read the integer input
+        int n = Integer.parseInt(reader.readLine());
+
+        // TODO: Increment n by 1
+        n++;
+        // TODO: Decrement n by 2
+        n -= 2;
+        // TODO: Double the resulting value of n
+        n *= 2;
+        // Print the final value of n
+        System.out.println(n);
+    }
+}
