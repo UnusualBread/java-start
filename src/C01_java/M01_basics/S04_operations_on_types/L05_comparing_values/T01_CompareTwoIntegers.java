d0cd60b31ddbcffeb9e2db637a74b0f0fba7c6dd
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-27 09:16:23 +0300

    Add file that compares two integers

---
+++ src/C01_java/M01_basics/S04_operations_on_types/L05_comparing_values/T01_CompareTwoIntegers.java
@@ -0,0 +1,28 @@
+package C01_java.M01_basics.S04_operations_on_types.L05_comparing_values;
+
+import java.util.Scanner;
+
+/*
+In Java, write a program that reads two integer values from the user and
+compares them. If the first number is greater than the second, print "TRUE",
+else print "FALSE". You should first scan an integer 'a' and then scan an
+integer 'b'.
+*/
+
+public class T01_CompareTwoIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstInteger = scanner.nextInt();
+        int secondInteger = scanner.nextInt();
+
+        boolean isGreater = firstInteger > secondInteger;
+
+        if (isGreater) {
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE");
+        }
+
+        scanner.close();
+    }
+}
