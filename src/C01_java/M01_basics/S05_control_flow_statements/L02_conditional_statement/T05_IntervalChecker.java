0bebd20017c558f7084ef1ed11201c5ab7990faf
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-28 00:19:09 +0300

    Correct the spelling error in the task comment

--- src/C01_java/M01_basics/S05_control_flow_statements/L02_conditional_statement/T05_IntervalChecker.java
+++ src/C01_java/M01_basics/S05_control_flow_statements/L02_conditional_statement/T05_IntervalChecker.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /*
 Given an integer as an input, print True if its value falls within the interval
-(−15,12]∪(14,17)∪[19,+∞). Otherwise, print False (case sensitive).
+(−15,12]∪(14,17)∪[19,+∞). Otherwise, print False (case-sensitive).
 */
 
 public class T05_IntervalChecker {
