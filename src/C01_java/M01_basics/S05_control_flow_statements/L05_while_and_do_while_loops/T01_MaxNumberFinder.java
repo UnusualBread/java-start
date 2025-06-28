8be862081da96611acbbf702879851f1f4a4e9c2
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-28 03:53:04 +0300

    Add a class that finds max number

---
+++ src/C01_java/M01_basics/S05_control_flow_statements/L05_while_and_do_while_loops/T01_MaxNumberFinder.java
@@ -0,0 +1,26 @@
+package C01_java.M01_basics.S05_control_flow_statements.L05_while_and_do_while_loops;
+
+import java.util.Scanner;
+
+/*
+Given a sequence of positive integer numbers (which ends with the number 0).
+Find the largest element of the sequence.
+*/
+
+public class T01_MaxNumberFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        int max = 0;
+
+        while (number != 0) {
+            max = Math.max(number, max);
+            number = scanner.nextInt();
+        }
+
+        System.out.println(max);
+
+        scanner.close();
+    }
+}
