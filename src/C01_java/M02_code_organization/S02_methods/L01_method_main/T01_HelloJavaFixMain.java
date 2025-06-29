57865e3fc5521254bf059eb89ca1bd8f4cf33802
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-29 08:36:47 +0300

    Add a fixed class that prints hello java

---
+++ src/C01_java/M02_code_organization/S02_methods/L01_method_main/T01_HelloJavaFixMain.java
@@ -0,0 +1,13 @@
+package C01_java.M02_code_organization.S02_methods.L01_method_main;
+
+/*
+Given a program that should print "Hello, Java!"
+but won't compile, fix the program.
+*/
+
+public class T01_HelloJavaFixMain {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Java!");
+    }
+}
