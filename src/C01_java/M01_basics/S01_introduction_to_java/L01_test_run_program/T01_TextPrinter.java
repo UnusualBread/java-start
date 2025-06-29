e6a48ad29bc7c971877184f37b6682fec3769932
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-30 00:21:49 +0300

    Rename first lesson folder to correct name

--- src/C01_java/M01_basics/S01_introduction_to_java/L01_test_run_program/T01_TextPrinter.java
+++ src/C01_java/M01_basics/S01_introduction_to_java/L01_test_run_program/T01_TextPrinter.java
@@ -1,4 +1,4 @@
-package C01_java.M01_basics.S01_introduction_to_java.L01_hello_world;
+package C01_java.M01_basics.S01_introduction_to_java.L01_test_run_program;
 
 public class T01_TextPrinter {
     public static void main(String[] args) {
