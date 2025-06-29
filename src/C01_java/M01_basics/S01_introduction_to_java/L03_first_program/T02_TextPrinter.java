a3974fc320fdb04a3f9c846ed09bfd0d5030b4ec
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-30 00:31:41 +0300

    Add a class that prints cats are robots

---
+++ src/C01_java/M01_basics/S01_introduction_to_java/L03_first_program/T02_TextPrinter.java
@@ -0,0 +1,12 @@
+package C01_java.M01_basics.S01_introduction_to_java.L03_first_program;
+
+/*
+Modify this program to output this text:
+Cats are robots!
+*/
+
+public class T02_TextPrinter {
+    public static void main(String[] args) {
+        System.out.println("Cats are robots!");
+    }
+}
