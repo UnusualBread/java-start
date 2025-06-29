6e727656c97bb6dca35a7bd36cf8d98581ce2141
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-30 02:52:26 +0300

    Add a fixed class that prints what it should

---
+++ src/C01_java/M02_code_organization/S01_code_style/L01_comments/T06_FixJavadocComment.java
@@ -0,0 +1,14 @@
+package C01_java.M02_code_organization.S01_code_style.L01_comments;
+
+/*
+Here's a code containing a Javadoc comment.
+Fix it, so that the output message would be printed.
+*/
+
+public class T06_FixJavadocComment {
+        /**
+         * It prints the text "It works correctly" to the standard output*/
+         public static void main(String[] args) {
+            System.out.println("It works correctly");
+     }
+ }
