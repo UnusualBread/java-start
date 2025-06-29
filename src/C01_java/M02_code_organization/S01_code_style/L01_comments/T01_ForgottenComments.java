46914f89a54a2ea87409b416728e548c026e6a0e
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-30 02:34:21 +0300

    Add a fixed class that compiles

---
+++ src/C01_java/M02_code_organization/S01_code_style/L01_comments/T01_ForgottenComments.java
@@ -0,0 +1,16 @@
+package C01_java.M02_code_organization.S01_code_style.L01_comments;
+
+/*
+Programmers wrote a code with a lot of comments, but they forgot to add the
+required symbol for comments. As a result, the code does not compile.
+You need to comment some lines in this code to make it compile.
+*/
+
+//The restriction: the class should have the name Main
+public class T01_ForgottenComments {
+
+    //The start point of your program
+    public static void main(String[] args) {
+        System.out.println("It compiles!"); //It prints "It compiles!"
+    }
+}
