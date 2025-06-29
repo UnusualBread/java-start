6be806e2393c6eb6bbd8ddde061ae3968d6e2cda
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-30 00:19:40 +0300

    Redo first lesson to what it should do

--- src/C01_java/M01_basics/S01_introduction_to_java/L01_hello_world/T01_TextPrinter.java
+++ src/C01_java/M01_basics/S01_introduction_to_java/L01_hello_world/T01_TextPrinter.java
@@ -1,7 +1,7 @@
 package C01_java.M01_basics.S01_introduction_to_java.L01_hello_world;
 
-public class T01_HelloWorld {
+public class T01_TextPrinter {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Java is powerful!");
     }
 }
