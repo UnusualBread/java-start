ef71aa89e2c4252f5255d7bed035189a8bbc60bc
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-30 04:18:29 +0300

    Add a class that prints renamed variables

---
+++ src/C01_java/M02_code_organization/S01_code_style/L02_naming_variables/T02_WeatherDataPrinter.java
@@ -0,0 +1,23 @@
+package C01_java.M02_code_organization.S01_code_style.L02_naming_variables;
+
+/*
+Refactor the following poorly named variables in a weather
+application to follow Java naming conventions:
+int t = 25;
+int h = 60;
+int w = 6;
+Print three lines with the refactored variable names and their values.
+*/
+
+public class T02_WeatherDataPrinter {
+    public static void main(String[] args) {
+        int airTemperature = 25;
+        int airHumidity = 60;
+        int windSpeed = 6;
+
+        // TODO: Refactor variable names and print the results
+        System.out.println("airTemperature = " + airTemperature);
+        System.out.println("airHumidity = " + airHumidity);
+        System.out.println("windSpeed = " + windSpeed);
+    }
+}
