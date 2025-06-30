569096048abe21f37e03aeea76c179187af84df5
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-06-30 05:53:24 +0300

    Add a class that calculates nuts that are left

--- src/C01_java/M01_basics/S04_operations_on_types/L01_integer_types_and_operations/T08_NutsCalculator_02.java
+++ src/C01_java/M01_basics/S04_operations_on_types/L01_integer_types_and_operations/T08_NutsCalculator_02.java
@@ -2,20 +2,21 @@ package C01_java.M01_basics.S04_operations_on_types.L01_integer_types_and_operat
 
 /*
 N squirrels found K nuts and decided to divide them equally.
-Determine how many nuts each squirrel will get.
+Find how many nuts will be left after each of the squirrels
+takes the equal amount of nuts.
 */
 
 import java.util.Scanner;
 
-public class T07_NutsCalculator {
+public class T08_NutsCalculator_02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int squirrelsNumber = sc.nextInt();
         int nutsNumber = sc.nextInt();
 
-        int nutsForOne = nutsNumber / squirrelsNumber;
+        int nutsLeft = nutsNumber % squirrelsNumber;
 
-        System.out.println(nutsForOne);
+        System.out.println(nutsLeft);
 
         sc.close();
     }
