b15693ef39835505eeea16dbbccdc2f77370ef07
UnusualBread <dvoeglazov_artemiy03@mail.ru>
2025-07-06 13:32:04 +0300

    Add a class that gives a compliment to the input person

---
+++ src/C01_java/M02_code_organization/S02_methods/L05_calling_methods/T02_ComplimentGiver.java
@@ -0,0 +1,23 @@
+package C01_java.M02_code_organization.S02_methods.L05_calling_methods;
+
+/*
+Here's the method callMeAwesome. Write the
+invocation of this method in the template below.
+*/
+
+import java.util.Scanner;
+
+public class T02_ComplimentGiver {
+
+    public static void main(String[] args) {
+        // call the method here
+        callMeAwesome();
+    }
+
+    // Do not change code below
+    public static void callMeAwesome() {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println(name + ", you're awesome!");
+    }
+}
