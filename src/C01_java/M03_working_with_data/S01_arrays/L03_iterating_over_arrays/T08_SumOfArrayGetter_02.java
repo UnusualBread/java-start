ec9c601f77ef33fa208b1bbab1fb9c36716cd2aa
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-07-08 12:52:41 +0300

    Add a class that gets sum of array's elements above an input number

---
+++ src/C01_java/M03_working_with_data/S01_arrays/L03_iterating_over_arrays/T08_SumOfArrayGetter_02.java
@@ -0,0 +1,34 @@
+package C01_java.M03_working_with_data.S01_arrays.L03_iterating_over_arrays;
+
+/*
+Write a program that reads an array of ints and an integer number n.
+The program must sum all the array elements greater than n.
+*/
+
+import java.util.Scanner;
+
+public class T08_SumOfArrayGetter_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalNumbers = sc.nextInt();
+
+        int[] arr = new int[totalNumbers];
+
+        for (int i = 0; i < totalNumbers; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int threshold = sc.nextInt();
+        int sum = 0;
+
+        for (int number : arr) {
+            if (number > threshold) {
+                sum += number;
+            }
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
