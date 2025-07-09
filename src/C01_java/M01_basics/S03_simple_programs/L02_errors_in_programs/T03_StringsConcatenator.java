aa460456bee70c8df765def75f62cd4ca613587d
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-07-09 14:56:38 +0300

    feat: add class that concatenates input strings in one

---
+++ src/C01_java/M01_basics/S03_simple_programs/L02_errors_in_programs/T03_StringsConcatenator.java
@@ -0,0 +1,24 @@
+package C01_java.M01_basics.S03_simple_programs.L02_errors_in_programs;
+
+/*
+You are given a program that should read three strings,
+concatenate them and output the result.
+For some reason, the program doesn't compile.
+Fix all compile-time errors as well as logic-errors.
+*/
+
+import java.util.Scanner;
+
+public class T03_StringsConcatenator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.next();
+        String b = scanner.next();
+        String c = scanner.next();
+
+        String result = a + b + c;
+
+        System.out.println(result);
+    }
+}
