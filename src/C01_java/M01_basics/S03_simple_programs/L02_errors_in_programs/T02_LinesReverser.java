418b7c14bc4f91ab7ee9e5b946151d3ea1418dd8
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-07-09 14:53:16 +0300

    feat: add class that reverses input lines

---
+++ src/C01_java/M01_basics/S03_simple_programs/L02_errors_in_programs/T02_LinesReverser.java
@@ -0,0 +1,21 @@
+package C01_java.M01_basics.S03_simple_programs.L02_errors_in_programs;
+
+/*
+Given a program that reads two lines and outputs them in another order.
+Now the program cannot compile.
+Fix all compile-time errors as well as logic-errors.
+*/
+
+import java.util.Scanner;
+
+public class T02_LinesReverser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String line1 = scanner.nextLine();
+        String line2 = scanner.nextLine();
+
+        System.out.println(line2);
+        System.out.println(line1);
+    }
+}
