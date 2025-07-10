d7c2abe159b08c98d3849e0439417a4788793324
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-07-10 17:35:07 +0300

    feat: add class Account

--- src/C01_java/M02_code_organization/S03_object_oriented_programming/MM01_classes_and_objects/SS01_classes_and_members/L05_defining_classes/T06_Account.java
+++ src/C01_java/M02_code_organization/S03_object_oriented_programming/MM01_classes_and_objects/SS01_classes_and_members/L05_defining_classes/T06_Account.java
@@ -1,4 +1,12 @@
 package C01_java.M02_code_organization.S03_object_oriented_programming.MM01_classes_and_objects.SS01_classes_and_members.L05_defining_classes;
 
-public class T06_Account {
+/*
+Define a class named Account. It must have three fields:
+a long field balance, a string field ownerName, and a boolean flag locked.
+*/
+
+class T06_Account {
+    long balance;
+    String ownerName;
+    boolean locked;
 }
