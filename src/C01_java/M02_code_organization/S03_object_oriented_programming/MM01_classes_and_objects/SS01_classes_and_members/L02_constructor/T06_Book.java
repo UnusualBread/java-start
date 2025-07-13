0dc68e5d7d171c8f570004c6dd6784b56cd1942a
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-07-13 13:57:08 +0300

    feat: add class Book

---
+++ src/C01_java/M02_code_organization/S03_object_oriented_programming/MM01_classes_and_objects/SS01_classes_and_members/L02_constructor/T06_Book.java
@@ -0,0 +1,20 @@
+package C01_java.M02_code_organization.S03_object_oriented_programming.MM01_classes_and_objects.SS01_classes_and_members.L02_constructor;
+
+/*
+Write a class named Book. It must have three fields: a string field title,
+an int field yearOfPublishing, an array of strings authors and a constructor
+to initialize these fields. The order of parameters in the constructor must be
+the same as presented above.
+*/
+
+class Book {
+    String title;
+    int yearsOfPublishing;
+    String authors;
+
+    public Book(String title, int yearsOfPublishing, String authors) {
+        this.title = title;
+        this.yearsOfPublishing = yearsOfPublishing;
+        this.authors = authors;
+    }
+}
