bd28637625185296f2ebea48625a8ca7ee07bddf
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-07-18 00:37:02 +0300

    feat: add class person

---
+++ src/C01_java/M02_code_organization/S03_object_oriented_programming/MM02_class_hierarchies/SS01_building_class_hierarchies/L01_inheritance/T01_Person.java
@@ -0,0 +1,23 @@
+package C01_java.M02_code_organization.S03_object_oriented_programming.MM02_class_hierarchies.SS01_building_class_hierarchies.L01_inheritance;
+
+/*
+In this problem, you need to develop a class hierarchy for the Hospital System.
+The root of the hierarchy is the Person class. It represents all employees and
+patients of the Hospital. Doctors are a kind of employee.
+*/
+
+class Person {
+
+}
+
+class Employee extends Person{
+
+}
+
+class Doctor extends Employee{
+
+}
+
+class Patient extends Person{
+
+}
