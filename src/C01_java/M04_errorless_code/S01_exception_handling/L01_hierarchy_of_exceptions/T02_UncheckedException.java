16c89737229304a3002730f7b88827735484be7f
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-07-27 16:32:43 +0300

    feat: add exception class

---
+++ src/C01_java/M04_errorless_code/S01_exception_handling/L01_hierarchy_of_exceptions/T02_UncheckedException.java
@@ -0,0 +1,17 @@
+package C01_java.M04_errorless_code.S01_exception_handling.L01_hierarchy_of_exceptions;
+
+/*
+Create a class named UncheckedException, which must inherit from an unchecked
+exception. Inside the UncheckedException class declare a no-argument constructor.
+*/
+
+class UncheckedException extends RuntimeException{
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        UncheckedException uncheckedException = new UncheckedException();
+        System.out.println(uncheckedException instanceof RuntimeException);
+    }
+}
