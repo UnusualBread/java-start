9cc530110f0b9f3d9492d15ea65e8dc3d7e4aa95
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-07-31 14:46:03 +0300

    feat: add class that creates string of english alphabet

---
+++ src/C01_java/M01_basics/S06_strings/L03_stringbuilder/T04_StringModifier.java
@@ -0,0 +1,25 @@
+package C01_java.M01_basics.S06_strings.L03_stringbuilder;
+
+/*
+Here you need to prepare a StringBuilder that produces all English letters in
+uppercase (A-Z). All the letters should be separated by one space, but there
+shouldn't be a space after the last letter.
+*/
+
+class EnglishAlphabet {
+
+    public static StringBuilder createEnglishAlphabet() {
+        // write your code here
+        StringBuilder sb = new StringBuilder(52);
+
+        for (char c = 'A'; c <= 'Z'; c++) {
+            sb.append(c);
+
+            if (c != 'Z') {
+                sb.append(' ');
+            }
+        }
+
+        return sb;
+    }
+}
