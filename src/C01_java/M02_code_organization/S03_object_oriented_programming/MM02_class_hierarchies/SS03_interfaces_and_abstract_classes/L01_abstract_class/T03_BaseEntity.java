e1feffbb8a220e253a8ec74457cec31f0da26ba2
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-07-31 20:07:20 +0300

    feat: add class BaseEntity

---
+++ src/C01_java/M02_code_organization/S03_object_oriented_programming/MM02_class_hierarchies/SS03_interfaces_and_abstract_classes/L01_abstract_class/T03_BaseEntity.java
@@ -0,0 +1,82 @@
+package C01_java.M02_code_organization.S03_object_oriented_programming.MM02_class_hierarchies.SS03_interfaces_and_abstract_classes.L01_abstract_class;
+
+/*
+You are writing an application which collects information about what sites were
+visited by what users. There are three classes: User, WebSite and Visit. Many
+fields and methods of these classes are the same.
+
+Write a new base abstract class named BaseEntity. Provided classes must extend
+it. Move all repeating fields and methods to the new class.
+*/
+
+abstract class BaseEntity {
+
+    protected long id;
+
+    protected long version;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+}
+
+class User extends BaseEntity {
+
+    protected String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Visit extends BaseEntity {
+
+    protected User user;
+
+    protected WebSite site;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public WebSite getSite() {
+        return site;
+    }
+
+    public void setSite(WebSite site) {
+        this.site = site;
+    }
+}
+
+class WebSite extends BaseEntity {
+
+    protected String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
