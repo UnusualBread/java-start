beda1c452b8a2a2df34230fffc978e6e72d8482c
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-08-01 16:34:16 +0500

    feat: add class that prints name of the constant

---
+++ src/C01_java/M02_code_organization/S03_object_oriented_programming/MM03_other_concepts/SS01_enumerations/L02_fields_and_methods_in_enum/T01_ConstantNameGetter.java
@@ -0,0 +1,25 @@
+package C01_java.M02_code_organization.S03_object_oriented_programming.MM03_other_concepts.SS01_enumerations.L02_fields_and_methods_in_enum;
+
+/*
+You are given an enum SI with three constants. Each constant stores a string
+field with its quantity name.
+Declare the instance method getQuantityName that returns the quantity name of
+the constant.
+*/
+
+enum SI {
+
+    M("length"),
+    KG("mass"),
+    S("time");
+
+    public final String quantityName;
+
+    SI(String quantityName) {
+        this.quantityName = quantityName;
+    }
+
+    String getQuantityName() {
+        return quantityName;
+    }
+}
