a8677e88f65fb2343fcafd03b6c79db4dc15c33a
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-08-01 15:35:36 +0500

    feat: add class that prints all days of the week

---
+++ src/C01_java/M02_code_organization/S03_object_oriented_programming/MM03_other_concepts/SS01_enumerations/L01_combining_constants_with_enum/T02_DayOfWeek.java
@@ -0,0 +1,24 @@
+package C01_java.M02_code_organization.S03_object_oriented_programming.MM03_other_concepts.SS01_enumerations.L01_combining_constants_with_enum;
+
+/*
+Declare an enum named DayOfWeek. It should include all days of the week in
+uppercase: SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY.
+*/
+
+enum DayOfWeek {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
+
+public class T02_DayOfWeek {
+    public static void main(String[] args) {
+        for (DayOfWeek day : DayOfWeek.values()) {
+            System.out.println(day);
+        }
+    }
+}
