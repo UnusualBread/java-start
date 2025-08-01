87acc888637ec4b14a9aefb214651361c6a0d87f
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-08-01 17:43:59 +0500

    feat: add class that performs two operations on input money

---
+++ src/C01_java/M02_code_organization/S03_object_oriented_programming/MM03_other_concepts/SS01_enumerations/L02_fields_and_methods_in_enum/T03_MoneyOperationsPerformer.java
@@ -0,0 +1,93 @@
+package C01_java.M02_code_organization.S03_object_oriented_programming.MM03_other_concepts.SS01_enumerations.L02_fields_and_methods_in_enum;
+
+/*
+Your task is to implement the method changeBalance to control balances.
+    - If the given operation is DEPOSIT the method adds the sum to the
+      account's balance.
+    - If the operation is WITHDRAW the method subtracts the given sum from the
+      account's balance. If the given sum is greater than the balance, the
+      method shouldn't change it. Instead, the method must output the string
+      "Not enough money to withdraw." (without quotes, don't forget the full
+      point).
+    - In any case, the method returns a boolean value — true if the balance has
+      changed, false if it hasn't.
+*/
+
+import java.util.Scanner;
+
+public class T03_MoneyOperationsPerformer {
+
+    /**
+     * The method change the balance of the given account according to an operation.
+     * @param account
+     * @param operation
+     * @return true if the balance has changed, otherwise - false.
+     */
+    public static boolean changeBalance(Account account, Operation operation, Long sum) {
+        boolean isChanged = switch (operation) {
+            case DEPOSIT -> {
+                account.balance += sum;
+                yield true;
+            }
+
+            case WITHDRAW -> {
+                if (sum > account.balance) {
+                    System.out.println("Not enough money to withdraw.");
+                    yield false;
+                } else {
+                    account.balance -= sum;
+                    yield true;
+                }
+            }
+        };
+
+        return isChanged;
+    }
+
+    /* Do not change code below */
+    enum Operation {
+        /**
+         * deposit (add) an amount into an Account
+         */
+        DEPOSIT,
+        /**
+         * withdraw (subtract) an amount from an Account
+         */
+        WITHDRAW
+    }
+
+    static class Account {
+
+        private String code;
+        private Long balance;
+
+        public String getCode() {
+            return code;
+        }
+
+        public Long getBalance() {
+            return balance;
+        }
+
+        public void setBalance(Long balance) {
+            this.balance = balance;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] parts = scanner.nextLine().split("\\s+");
+
+        Account account = new Account();
+        account.setBalance(Long.parseLong(parts[0]));
+
+        Operation operation = Operation.valueOf(parts[1]);
+
+        Long sum = Long.parseLong(parts[2]);
+
+        if (changeBalance(account, operation, sum)) {
+            System.out.println(account.getBalance());
+        }
+    }
+}
