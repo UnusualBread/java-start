bd31d3600c11ab56d7425c75600b1959e63c24bc
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-08-03 02:48:13 +0500

    feat: add class that cuts middle part of input string

---
+++ src/C01_java/M01_basics/S06_strings/L02_processing_strings/T16_MiddleOfStringCutter.java
@@ -0,0 +1,28 @@
+package C01_java.M01_basics.S06_strings.L02_processing_strings;
+
+/*
+Write a program that reads a string, and then outputs the string without its
+middle character when the length is odd, and without the middle 2 characters
+when the length is even.
+*/
+
+import java.util.Scanner;
+
+public class T16_MiddleOfStringCutter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        sc.close();
+
+        String result;
+        int length = str.length();
+
+        if (length % 2 == 0) {
+            result = str.substring(0, length / 2 - 1) + str.substring(length / 2 + 1);
+        } else {
+            result = str.substring(0, length / 2) + str.substring(length / 2 + 1);
+        }
+
+        System.out.println(result);
+    }
+}
