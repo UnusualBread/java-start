efd6ae437a970be426a56a7c0a8f0d16d8be5860
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-11-08 06:25:19 +0300

    feat: add class that catchs and prints RuntimeEexception

---
+++ src/theory/M04_errorless_code/S01_exception_handling/L06_throwing_exceptions/T01_ExceptionThrower.java
@@ -0,0 +1,19 @@
+package theory.M04_errorless_code.S01_exception_handling.L06_throwing_exceptions;
+
+public class T01_ExceptionThrower {
+
+    public static void method() {
+        throw new RuntimeException("Hello");
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        try {
+            method();
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException");
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
+}
