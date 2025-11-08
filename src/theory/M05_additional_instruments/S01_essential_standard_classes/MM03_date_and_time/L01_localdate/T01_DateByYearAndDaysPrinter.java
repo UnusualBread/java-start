e8f4882dc00b8b4ce05da8c57135be02e9474328
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-11-08 21:08:37 +0300

    feat: add class that prints date by given year and day

---
+++ src/theory/M05_additional_instruments/S01_essential_standard_classes/MM03_date_and_time/L01_localdate/T01_DateByYearAndDaysPrinter.java
@@ -0,0 +1,25 @@
+package theory.M05_additional_instruments.S01_essential_standard_classes.MM03_date_and_time.L01_localdate;
+
+/*
+Write a program that reads a year and three days of this year (by their
+numbers) from the standard input and output all dates corresponding to these
+numbers in the same order.
+*/
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class T01_DateByYearAndDaysPrinter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        int[] dayNumbers = new int[3];
+        for (int i = 0; i < dayNumbers.length; i++) {
+            dayNumbers[i] = sc.nextInt();
+        }
+
+        for (int dayNumber : dayNumbers) System.out.println(LocalDate.ofYearDay(year, dayNumber));
+    }
+}
