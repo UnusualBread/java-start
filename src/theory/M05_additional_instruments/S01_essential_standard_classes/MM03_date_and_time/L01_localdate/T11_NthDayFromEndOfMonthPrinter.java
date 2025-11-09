9158b6c16ebd52d5f21cec40ae29fa542e1783a0
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-11-09 22:54:37 +0300

    feat: add class that prints Nth day from end of month

---
+++ src/theory/M05_additional_instruments/S01_essential_standard_classes/MM03_date_and_time/L01_localdate/T11_NthDayFromEndOfMonthPrinter.java
@@ -0,0 +1,26 @@
+package theory.M05_additional_instruments.S01_essential_standard_classes.MM03_date_and_time.L01_localdate;
+
+/*
+Write a program that prints the n-th day from the end of a month.
+
+The program must read the year, the month, and the remaining number of days
+till the end of the month from standard input and then output the date.
+*/
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class T11_NthDayFromEndOfMonthPrinter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+        int month = sc.nextInt();
+        int dayNumber = sc.nextInt();
+
+        LocalDate startDate = LocalDate.of(year, month, 1);
+        int day = startDate.lengthOfMonth() - dayNumber + 1;
+
+        System.out.println(LocalDate.of(year, month, day));
+    }
+}
