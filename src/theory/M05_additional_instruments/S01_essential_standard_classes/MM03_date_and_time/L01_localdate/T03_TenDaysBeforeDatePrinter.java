719816ecbcc8a9fef9c3831a8bb7c5865d8a28d6
UnusualBread <dvoeglazov_artemiy03@mail.ru>
2025-11-09 06:35:12 +0300

    feat: add class that prints date 10 days before given date

---
+++ src/theory/M05_additional_instruments/S01_essential_standard_classes/MM03_date_and_time/L01_localdate/T03_TenDaysBeforeDatePrinter.java
@@ -0,0 +1,15 @@
+package theory.M05_additional_instruments.S01_essential_standard_classes.MM03_date_and_time.L01_localdate;
+
+/*
+Write a program that reads a date from the standard input and prints a date
+that is 10 days before.
+*/
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class T03_TenDaysBeforeDatePrinter {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.parse(new Scanner(System.in).nextLine()).minusDays(10));
+    }
+}
