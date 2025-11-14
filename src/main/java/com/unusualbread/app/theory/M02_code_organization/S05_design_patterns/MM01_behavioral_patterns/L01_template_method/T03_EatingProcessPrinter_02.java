32ae1dfa95bdd4fc702099920b9cf982109f998b
UnusualBread <dvoeglazov_artemiy03@mail.ru>
2025-11-14 06:32:41 +0300

    feat: add class that prints meal process depending on input

--- src/main/java/com/unusualbread/app/theory/M02_code_organization/S05_design_patterns/MM01_behavioral_patterns/L01_template_method/T03_EatingProcessPrinter_02.java
+++ src/main/java/com/unusualbread/app/theory/M02_code_organization/S05_design_patterns/MM01_behavioral_patterns/L01_template_method/T03_EatingProcessPrinter_02.java
@@ -1,14 +1,25 @@
 package com.unusualbread.app.theory.M02_code_organization.S05_design_patterns.MM01_behavioral_patterns.L01_template_method;
 
+/*
+Now, we'll add a new concrete class Sandwich.
+Sandwich is different from Steak, that's why you need to implement it.
+*/
+
 import java.util.Scanner;
 
-abstract class Meal {
+abstract class Meal2 {
 
     /**
      * It provides template method of meal routine.
      */
     public void doMeal() {
-        // write your code here ...
+        prepareIngredients();
+
+        cook();
+
+        eat();
+
+        cleanUp();
     }
 
     public abstract void prepareIngredients();
@@ -22,7 +33,7 @@ abstract class Meal {
     public abstract void cleanUp();
 }
 
-class Steak extends Meal {
+class Steak2 extends Meal2 {
 
     @Override
     public void prepareIngredients() {
@@ -40,21 +51,35 @@ class Steak extends Meal {
     }
 }
 
-class Sandwich extends Meal {
-    // write your code here ...
+class Sandwich2 extends Meal2 {
+
+    @Override
+    public void prepareIngredients() {
+        System.out.println("Ingredients: bacon, white bread, egg, cheese, mayonnaise, tomato");
+    }
+
+    @Override
+    public void cook() {
+        System.out.println("Paste ingredients between bread slices. Toast sandwich");
+    }
+
+    @Override
+    public void cleanUp() {
+        System.out.println("Lick fingers and go to sleep");
+    }
 }
 
-public class Main {
+public class T03_EatingProcessPrinter_02 {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         final String order = scanner.nextLine();
         scanner.close();
-        Meal meal = null;
+        Meal2 meal = null;
         if ("Sandwich".equals(order)) {
-            meal = new Sandwich();
+            meal = new Sandwich2();
             meal.doMeal();
         } else if ("Steak".equals(order)) {
-            meal = new Steak();
+            meal = new Steak2();
             meal.doMeal();
         } else {
             System.out.println("Error");
