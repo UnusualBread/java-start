a550d64c1f3757b59d516debbea681b4de078381
UnusualBread <dvoeglazov_artemiy03@mail.ru>
2025-11-15 07:47:30 +0300

    feat: add class that prints passed value

---
+++ src/main/java/com/unusualbread/app/theory/M02_code_organization/S06_generic_programming/MM01_generics_essentials/L01_introduction_to_generic_programming/T01_PassedValuePrinter.java
@@ -0,0 +1,27 @@
+package com.unusualbread.app.theory.M02_code_organization.S06_generic_programming.MM01_generics_essentials.L01_introduction_to_generic_programming;
+
+/*
+Correct the code to make it compilable. The program should print:
+"Passed value: value"
+*/
+
+class T01_PassedValuePrinter {
+    public static void main(String... args) {
+        Printer<String> printer = new Printer<>();
+
+        printer.set("value");
+        printer.print();
+    }
+}
+
+class Printer<T> {
+    private T value;
+
+    void set(T value) {
+        this.value = value;
+    }
+
+    void print() {
+        System.out.println("Passed value: " + value);
+    }
+}
