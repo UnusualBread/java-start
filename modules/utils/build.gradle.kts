0ef25990d1b3dcca18c327c3fe0a89a2ec09bf10
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-11-21 12:47:28 +0300

    chore: add test dependencies and task test

--- modules/utils/build.gradle.kts
+++ modules/utils/build.gradle.kts
@@ -8,3 +8,13 @@ java {
     }
     modularity.inferModulePath.set(true)
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.14.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
