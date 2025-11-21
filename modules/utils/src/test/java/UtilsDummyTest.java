c677948d166659410f381a41a43eba07e7269524
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-11-21 13:46:53 +0300

    refactor: add annotation '@DisplayName' to test class

--- modules/utils/src/test/java/UtilsDummyTest.java
+++ modules/utils/src/test/java/UtilsDummyTest.java
@@ -1,7 +1,9 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UtilsDummyTest {
     @Test
+    @DisplayName("ignore me")
     void testUtils() {
         assert true;
     }
