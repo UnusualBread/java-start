8ac7c94c3607e37ed86d6b3248879ebc3d841891
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-11-21 12:41:29 +0300

    style: simplify test annotations by using @Test instead of full names

--- src/test/java/com/unusualbread/app/theory/M04_errorless_code/S03_testing_tools_and_libraries/L01_unit_testing_with_junit/CalculatorTest.java
+++ src/test/java/com/unusualbread/app/theory/M04_errorless_code/S03_testing_tools_and_libraries/L01_unit_testing_with_junit/CalculatorTest.java
@@ -1,10 +1,12 @@
 package com.unusualbread.app.theory.M04_errorless_code.S03_testing_tools_and_libraries.L01_unit_testing_with_junit;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void add() {
         Calculator calculator = new Calculator();
         int result = calculator.add(1, 2);
@@ -12,7 +14,7 @@ public class CalculatorTest {
         assertEquals(3, result);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void subtract() {
         Calculator calculator = new Calculator();
         int result = calculator.subtract(1, 2);
@@ -20,7 +22,7 @@ public class CalculatorTest {
         assertEquals(-1, result);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void multiply() {
         Calculator calculator = new Calculator();
         int result = calculator.multiply(1, 2);
@@ -28,7 +30,7 @@ public class CalculatorTest {
         assertEquals(2, result);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void divide() {
         Calculator calculator = new Calculator();
         int result = calculator.divide(1, 2);
