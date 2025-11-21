da00b3a9f43b23190e894bb463ddbf56ca24003a
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-11-21 18:54:43 +0300

    feat: add class Adder with unit test class

--- src/main/java/com/unusualbread/app/theory/M04_errorless_code/S03_testing_tools_and_libraries/L01_unit_testing_with_junit/T02_Adder.java
+++ src/main/java/com/unusualbread/app/theory/M04_errorless_code/S03_testing_tools_and_libraries/L01_unit_testing_with_junit/T02_Adder.java
@@ -1,4 +1,16 @@
 package com.unusualbread.app.theory.M04_errorless_code.S03_testing_tools_and_libraries.L01_unit_testing_with_junit;
 
+/*
+Suppose you have the Adder class.
+Implement the corresponding test method using the Assertions.assertEquals(int
+expected, int actual) static method and a suitable test case at your discretion
+to verify that the add method of the Adder class works correctly. Please
+do not add any annotations or import any packages.
+*/
+
 public class T02_Adder {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
--- src/test/java/com/unusualbread/app/theory/M04_errorless_code/S03_testing_tools_and_libraries/L01_unit_testing_with_junit/T02_AdderTest.java
+++ src/test/java/com/unusualbread/app/theory/M04_errorless_code/S03_testing_tools_and_libraries/L01_unit_testing_with_junit/T02_AdderTest.java
@@ -1,12 +1,23 @@
 package com.unusualbread.app.theory.M04_errorless_code.S03_testing_tools_and_libraries.L01_unit_testing_with_junit;
 
-import org.junit.jupiter.api.Test;
+/*
+Suppose you have the Adder class.
+Implement the corresponding test method using the Assertions.assertEquals(int
+expected, int actual) static method and a suitable test case at your discretion
+to verify that the add method of the Adder class works correctly. Please
+do not add any annotations or import any packages.
+*/
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class T02_AdderTest {
 
     @Test
     void add() {
+        T02_Adder adder = new T02_Adder();
+        int result = adder.add(1, 2);
+
+        Assertions.assertEquals(3, result);
     }
 }
