0759db74f3e7cef3e22e6e0c3af7c213ead37057
UnusualBread <dvoeglazov_artemiy03@mail.ru>
2025-12-21 04:25:35 +0300

    build: add assertj dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ allprojects {
 dependencies {
     implementation(project(":utils"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.14.1")
+    testImplementation("org.assertj:assertj-core:3.21.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
