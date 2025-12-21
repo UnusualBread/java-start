095a7fc8a9861de0ef865f98565fef0670b80a78
UnusualBread <dvoeglazov_artemiy03@mail.ru>
2025-12-21 03:29:38 +0300

    build: simplify JUnit dependencies to single artifact and remove redundant jdk version stating

--- modules/utils/build.gradle.kts
+++ modules/utils/build.gradle.kts
@@ -3,15 +3,11 @@ plugins {
 }
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(23))
-    }
     modularity.inferModulePath.set(true)
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.14.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.14.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
