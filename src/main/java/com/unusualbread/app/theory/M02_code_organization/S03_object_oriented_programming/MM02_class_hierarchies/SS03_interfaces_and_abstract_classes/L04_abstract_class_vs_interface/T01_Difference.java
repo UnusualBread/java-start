c2ab183e9fc7d6931272f629dcf964ee44ad9dbf
UnusualBread <dvoeglazov_artemiy03@mail.ru>
2025-12-25 12:37:40 +0300

    feat: add class with common difference between abstract classes and interfaces

---
+++ src/main/java/com/unusualbread/app/theory/M02_code_organization/S03_object_oriented_programming/MM02_class_hierarchies/SS03_interfaces_and_abstract_classes/L04_abstract_class_vs_interface/T01_Difference.java
@@ -0,0 +1,30 @@
+package com.unusualbread.app.theory.M02_code_organization.S03_object_oriented_programming.MM02_class_hierarchies.SS03_interfaces_and_abstract_classes.L04_abstract_class_vs_interface;
+
+public class T01_Difference {
+    /*
+    1) an abstract class can have abstract and non-abstract instance
+       methods while an interface can have abstract or default instance methods;
+
+    2) an abstract class can extend another abstract or regular class and an
+       interface can only extend another interface;
+
+    3) an abstract class can extend only one class while an interface can
+       extend any number of interfaces;
+
+    4) an abstract class can have final, non-final, static, non-static
+       variables (regular fields) while an interface can only have static final
+       variables;
+
+    5) an abstract class can provide an implementation of an interface but an
+       interface cannot provide an implementation of an abstract class;
+
+    6) an abstract class can have a constructor and an interface cannot;
+
+    7) in an abstract class, the keyword abstract is mandatory to declare a
+       method as an abstract one while in an interface this keyword is optional;
+
+       Typically, interfaces are used to decouple the interface of a component
+       (class) from the implementation while abstract classes are often used as
+       base classes with common fields to be extended by subclasses.
+    */
+}
