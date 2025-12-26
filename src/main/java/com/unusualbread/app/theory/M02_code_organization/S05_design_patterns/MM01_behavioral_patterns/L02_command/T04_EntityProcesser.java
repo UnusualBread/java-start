6aa6ddb5cf57e7b1c29b1b5a321b739175b1dd6e
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2025-12-26 16:40:59 +0300

    feat: add command pattern realization of movable and storable entity

--- src/main/java/com/unusualbread/app/theory/M02_code_organization/S05_design_patterns/MM01_behavioral_patterns/L02_command/T04_EntityProcesser.java
+++ src/main/java/com/unusualbread/app/theory/M02_code_organization/S05_design_patterns/MM01_behavioral_patterns/L02_command/T04_EntityProcesser.java
@@ -1,5 +1,24 @@
 package com.unusualbread.app.theory.M02_code_organization.S05_design_patterns.MM01_behavioral_patterns.L02_command;
 
+/*
+Write two classes which represent the Command pattern, CommandMove and
+CommandPutItem.
+
+These classes should also support undo. They should implement the entity
+movement on the 2D and item putting. The x-axis on the map goes from left to
+right, the y-axis goes from bottom to top.
+
+The inventory of the entity has getInventoryLength() slots starting from 0, and
+each slot can contain only one item. The entity should put an item into a slot
+with the least possible index (slots with nothing inside is null). If all slots
+of an entity are filled with some item, then entity should not place this item
+in its inventory.
+
+To undo, you should revert the changes made to the entity, so the coordinates
+and the inventory of the entity before execute() and after undo() should be the
+same.
+*/
+
 interface Movable {
     int getX();
     int getY();
@@ -24,10 +43,12 @@ class CommandMove implements Command4 {
     int yMovement;
     int xOld;
     int yOld;
+    boolean saved = false;
 
     private void saveOld() {
         xOld = entity.getX();
         yOld = entity.getY();
+        saved = true;
     }
 
     private void setNew(int x, int y) {
@@ -37,17 +58,48 @@ class CommandMove implements Command4 {
 
     @Override
     public void execute() {
-        saveOld();
-        setNew(xMovement, yMovement);
+        if (!saved) {
+            saveOld();
+        }
+        setNew(xOld + xMovement, yOld + yMovement);
     }
 
     @Override
     public void undo() {
-        setNew(xOld, yOld);
+        if (saved) {
+            setNew(xOld, yOld);
+        }
     }
 }
 
 class CommandPutItem implements Command4 {
     Storable entity;
     String item;
+    int usedIndex = -1;
+
+    private void putItem() {
+        for (int i = 0; i < entity.getInventoryLength(); i++) {
+            if (entity.getInventoryItem(i) == null) {
+                entity.setInventoryItem(i, item);
+                usedIndex = i;
+                break;
+            }
+        }
+    }
+
+    private void removeItem() {
+        if (usedIndex != -1) {
+            entity.setInventoryItem(usedIndex, null);
+        }
+    }
+
+    @Override
+    public void execute() {
+        putItem();
+    }
+
+    @Override
+    public void undo() {
+        removeItem();
+    }
 }
