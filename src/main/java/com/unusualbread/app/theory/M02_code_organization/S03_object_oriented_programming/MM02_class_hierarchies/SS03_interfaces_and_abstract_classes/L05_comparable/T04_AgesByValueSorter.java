f88da58d672ebbcfb05993fcf1b62dc4901dc86e
UnusualBread <dvoeglazov_artemiy03@mail.ru>
2026-01-10 22:52:53 +0300

    feat: add class that sorts ages by value

---
+++ src/main/java/com/unusualbread/app/theory/M02_code_organization/S03_object_oriented_programming/MM02_class_hierarchies/SS03_interfaces_and_abstract_classes/L05_comparable/T04_AgesByValueSorter.java
@@ -0,0 +1,56 @@
+package com.unusualbread.app.theory.M02_code_organization.S03_object_oriented_programming.MM02_class_hierarchies.SS03_interfaces_and_abstract_classes.L05_comparable;
+
+/*
+A personal data processing system uses a class called Age instead of a
+primitive value to store a person's age. Change this class so that it is
+possible to compare Age objects.
+*/
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+    class Age implements Comparable<Age> {
+        private final int value;
+
+        public Age(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        @Override
+        public int compareTo(Age age) {
+            return Integer.compare(value, age.value);
+        }
+    }
+
+    class T04_ByAgeSorter {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            List<Age> list = Arrays.stream(sc.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .mapToObj(Age::new)
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            Checker2.check(list);
+        }
+    }
+
+    class Checker2 {
+        static void check(List<Age> list) {
+            for (int i = 1; i < list.size(); i++) {
+                var curr = list.get(i);
+                var prev = list.get(i - 1);
+                if (curr.getValue() < prev.getValue()) {
+                    System.out.println("Incorrect sorted order");
+                    return;
+                }
+            }
+            System.out.println("Correct sorted order");
+        }
+    }
