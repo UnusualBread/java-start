ad54876bad2b374a417bea7dd9666da752ce54c7
Артемий Двоеглазов <dvoeglazov_artemiy03@mail.ru>
2026-01-11 17:25:45 +0300

    chore: add mockito-junit-jupiter dependency for Mockito's annotations

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
 
     testImplementation("org.assertj:assertj-core:3.27.6")
     testImplementation("org.mockito:mockito-core:5.21.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.21.0")
 }
 
 repositories {
